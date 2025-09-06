1a49802a1072fca64aec5462e0bde2966c1f06e3
Maria <mariiia.shipova@gmail.com>
2025-09-06 18:32:17 +0500

    Добавлен второй парсер, изменен start.cmd
    Результат: выдает 10 вакансий, но только названия их. Выдает 10 заказов, ссылки и оплату (как и нужгно)

--- start.cmd
+++ start.cmd
@@ -2,7 +2,7 @@
 REM start.cmd - run the vacancies fetch script from project root
 cd /d "%~dp0"
 echo Running: node src/start.js %*
-node src/fetch-vacancies.js %*
+node src/start.js %*
 if %ERRORLEVEL% neq 0 (
   echo.
   echo Script exited with error %ERRORLEVEL%.
